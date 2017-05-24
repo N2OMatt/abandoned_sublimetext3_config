3bfb6f4d4f82092223efd6059576aaf28a49f33d
n2omatt <n2o.matt@gmail.com>
2017-05-24 18:15:01 -0300

    - Improve the script to works on Linux / Cygwin OSX.
    Put the configuration files into a separated folder.
    Add option to specify some platform options (like the big mac)
    Improve the install script.

--- install.sh
+++ install.sh
@@ -11,34 +11,94 @@
 ################################################################################
 
 
-## Constants...
+################################################################################
+## Constants                                                                  ##
+################################################################################
 WHO_AM_I="$(whoami)";
 
 OSX_PATH="$HOME/Library/Application Support/Sublime Text 3/Packages/User/"
 LINUX_PATH="$HOME/.config/sublime-text-3/Packages/User/";
 NT_PATH=/cygdrive/c/Users/matt/AppData/Roaming/Sublime\ Text\ 3/Packages/User/;
 
-## Variables..
+
+################################################################################
+## Variables                                                                  ##
+################################################################################
 HOST=$(uname -s);
 
 IS_ON_NT=$( echo $HOST | grep -i cygwin);
 IS_ON_OSX=$(echo $HOST | grep -i darwin);
 
-echo "--> Installing Sublime Text 3 configuration files...";
-echo "----> OS        : ($HOST)";
-echo "----> IS_ON_NT  : ($IS_ON_NT)";
-echo "----> IS_ON_OSX : ($IS_ON_OSX)";
+PLATFORM_OPTION="$1";
 
-## Copy the configuration to the correct path.
-if [ -n "$IS_ON_NT" ]; then
-    echo "----> Copying files to ($NT_PATH)";
-    cp -Rv ./config/* "$NT_PATH";
-elif [ -n "$IS_ON_OSX" ]; then
-    echo "----> Copying files to ($OSX_PATH)";
-    cp -Rv ./sublime_config/* "$OSX_PATH";
-else
-    echo "----> Copying files to ($LINUX_PATH)";
-    cp -Rv ./sublime_config/* "$LINUX_PATH";
+
+################################################################################
+## Functions                                                                  ##
+################################################################################
+replace_file_contents()
+{
+    STR=$1;
+    FILE_TO_INSERT=$2
+    FILE_TO_MODIFY=$3
+
+    ## Only to debug...
+    # echo "STR            $STR";
+    # echo "FILE_TO_INSERT $FILE_TO_INSERT";
+    # echo "FILE_TO_MODIFY $FILE_TO_MODIFY";
+
+    sed -i "BACKUP" -e "/$STR/r $FILE_TO_INSERT" -e "/$STR/d" $FILE_TO_MODIFY;
+    rm ${FILE_TO_MODIFY}BACKUP;
+}
+
+
+################################################################################
+## Script                                                                     ##
+################################################################################
+## Get any platform options that we may passed.
+if [ -z "$PLATFORM_OPTION" ]; then
+    PLATFORM_OPTION="default";
 fi;
+
+## Clean up the build directory.
+rm    -rf build;
+mkdir -p  build;
+
+## Log.
+echo "--> Installing Sublime Text 3 configuration files...";
+echo "----> OS              : ($HOST)";
+echo "----> IS_ON_NT        : ($IS_ON_NT)";
+echo "----> IS_ON_OSX       : ($IS_ON_OSX)";
+echo "----> PLATFORM_OPTION : ($PLATFORM_OPTION)";
+
+## Get the correct path.
+FINAL_PATH="";
+if [ -n "$IS_ON_NT" ]; then
+    FINAL_PATH="$NT_PATH";
+elif [ -n "$IS_ON_OSX" ]; then
+    FINAL_PATH="$OSX_PATH";
+else
+    FINAL_PATH="$LINUX_PATH";
+fi;
+
+## Copy everything needed to build path.
+echo "----> Copying files to build dir";
+cp -r ./config/files/* ./build;
+
+
+## Do the replacements needed to this platform.
+if [ ! -e "./config/preferences/$PLATFORM_OPTION.txt" ]; then
+    echo "PLATFORM_OPTION ($PLATFORM_OPTION) is not valid - Aborting...";
+    exit 1;
+fi;
+
+## STR - FILE TO INSERT - FILE TO MODIFY
+replace_file_contents "__FONT_CONFIG__"                         \
+                      ./config/preferences/$PLATFORM_OPTION.txt \
+                      ./build/Preferences.sublime-settings
+
+
+## Copy everything needed to build path.
+echo "----> Copying files to ($FINAL_PATH)";
+cp -Rv ./build/* "$FINAL_PATH";
 
 echo "--> Done...";
