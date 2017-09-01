51a395acb6f450108dd88771c2c2bb5224f1c0be
n2omatt <n2o.matt@gmail.com>
2017-09-01 11:41:31 -0300

    - Add license header and clean the file a bit.

--- config/files/insert_datetime.py
+++ config/files/insert_datetime.py
@@ -1,5 +1,25 @@
-import sublime, sublime_plugin, time
+##----------------------------------------------------------------------------##
+## File      : insert_datetime.py                                             ##
+## Project   : sublimetext3_config                                            ##
+## Author    : n2omatt <n2omatt@amazingcow.com>                               ##
+## Date      : August 30, 2017                                                ##
+## License   : GPLv3                                                          ##
+## Copyright : N2OMatt - Copyright (c) 2017                                   ##
+##                                                                            ##
+## Description:                                                               ##
+##                                                                            ##
+##----------------------------------------------------------------------------##
 
+################################################################################
+## Imports                                                                    ##
+################################################################################
+import time;
+import sublime;
+import sublime_plugin;
+
+################################################################################
+## Script                                                                     ##
+################################################################################
 class InsertDatetimeCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         sel = self.view.sel();
